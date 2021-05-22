69242bfe287a02fccbae435db2e5e2a836fc11e0
leeway.zlw <leeway.zlw@alibaba-inc.com>
2021-05-22 19:55:02 +0800

    1.0.0

--- src/content/index.ts
+++ src/content/index.ts
@@ -1,1 +1,77 @@
 console.log('Hey, the content script is running!');
+import "core-js/stable";
+import "regenerator-runtime/runtime";
+import $ from 'jquery';
+import JSZip from 'jszip';
+import Url from 'url-parse';
+import { saveAs } from 'file-saver';
+
+function parseQuery(queryString: string) {
+  const query: any = {};
+  var pairs = (queryString[0] === '?' ? queryString.substr(1) : queryString).split('&');
+  for (var i = 0; i < pairs.length; i++) {
+      var pair = pairs[i].split('=');
+      query[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+  }
+  return query;
+}
+
+const history: string[] = [];
+
+function saveHistory(urls: string[]) {
+  let changed = false;
+  for (const url of urls) {
+    if (!history.includes(url)) {
+      history.push(url);
+      changed = true;
+    }
+  }
+  return changed;
+}
+
+async function tickShot() {
+  let count = 0;
+  const zip = new JSZip();
+  zip.folder(document.title);
+  while (true) {
+    let historyLen = history.length;
+    $('.pageImageDisplay img').each((index, element) => {
+      const url = (element.getAttribute('src'));
+      if (url) {
+        saveHistory([url]);
+      }
+    });
+    $('.SPRITE_page_down').trigger('click');
+    await new Promise(resolve => setTimeout(resolve, 300));
+    if (history.length > historyLen) {
+      count = 0;
+      historyLen = history.length;
+      const url = new Url(history[historyLen - 1], true);
+      const blob = await fetch(url.href).then(res => res.blob());
+      const page = url.query.pg;
+      const ext = blob.type.split('/')[1];
+      zip.file(`${document.title}/${page}.${ext}`, blob);
+    } else {
+      count++;
+    }
+    if (count >= 10) {
+      zip.generateAsync({ type: 'blob' }).then(blob => {
+        saveAs(blob, document.title + '.zip');
+      });
+      break;
+    }
+  }
+}
+
+chrome.runtime.onMessage.addListener((msg, sender, res) => {
+  console.log(msg);
+  const { type } = msg;
+  switch (type) {
+    case 'start-shot':
+      tickShot().then(() => res({ type: 'start-shot', status: 'completed' }));
+      break;
+
+    default:
+      break;
+  }
+});
--- src/popup/App.vue
+++ src/popup/App.vue
@@ -1,26 +1,61 @@
 <template>
   <div class="container">
-    <h1>{{ message }}</h1>
+    <h2>{{message}}</h2>
+    <p>{{url}}</p>
+    <p>{{parsedUrl && parsedUrl.host}}</p>
+    <p>{{parsedUrl && parsedUrl.pathname}}</p>
+    <button :disabled="!isGBook || running" @click="startShot">{{running ? 'loading' : 'start'}}</button>
   </div>
 </template>
 
 <script lang="ts">
-export default {
-  name: 'App',
+import Vue from "vue";
+export default Vue.extend({
+  name: "App",
+  mounted() {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+      this.currentTab = tabs[0];
+      this.url = tabs[0].url || '';
+    });
+  },
+  methods: {
+    startShot() {
+      const id = this.currentTab?.id;
+      if (id) {
+        chrome.tabs.sendMessage(id, {type: 'start-shot'}, res => {
+          console.log(res);
+          this.running = false;
+        });
+        this.running = true;
+      }
+    }
+  },
+  computed: {
+    parsedUrl() {
+      return (this as any).url && new URL((this as any).url);
+    },
+    isGBook() {
+      return (this as any).parsedUrl.host.includes('books.google.com') &&
+      (this as any).parsedUrl.pathname === '/books'
+    }
+  },
   data() {
     return {
-      message: 'Hey, it is the Popup page running on Vue.js!',
+      message: "GBookShotter",
+      url: "",
+      currentTab: null as chrome.tabs.Tab | null,
+      running: false,
     };
   },
-};
+});
 </script>
 
 <style scoped lang="scss">
-  .container {
-    text-align: center;
+.container {
+  text-align: center;
 
-    h1 {
-      font-weight: 600;
-    }
+  h1 {
+    font-weight: 600;
   }
+}
 </style>
